ee7e30799c09ec151951f0d99c04042d59a95917
Ashishproponenttech <ashish@proponenttech.com>
2026-01-14 10:29:27 +0530

    Remove next/font to fix GitHub Pages 404 errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  basePath: isProd ? "/starter-react" : "",
+  assetPrefix: isProd ? "/starter-react/" : "",
 };
 
 export default nextConfig;
